4825d922171c1114d5f5cf0a4c939b06000509be
tzcoolman <tzcoolman@gmail.com>
2022-06-03 14:37:21 -0400

    Update DSCN_sn_similarity.py

--- DSCN_sn_similarity.py
+++ DSCN_sn_similarity.py
@@ -1,7 +1,7 @@
 import DSCN_loading as DSCN_load
 import DSCN_scorer_local_TN as DSCN_scorer
-
-
+import os,sys
+import networkx as nx
 #target_file="SL_synlethDB_pancrea.WG.list"
 #t_exp_file="tumor_exp.txt.WG"
 #t_fc_file="tumor.FC.WG"
@@ -29,7 +29,7 @@ t_exp_hash={}
 c_exp_hash={}
 cluster_list={}
 c_essen_hash={}
-t_fc_file={}
+t_fc_hash={}
 cell_graph=nx.Graph()
 target_list={}
 
@@ -53,55 +53,67 @@ def make_tc_matrix(graph,t_exp_hash,t_fc_hash):
 	return (t_matrix,c_matrix)
 
 def normalize_matrix(func_matrix):
-        abs_func_matrix=np.absolute(func_matrix)
-        total_col,total_row=func_matrix.shape()
-        row=0
-        while row<total_row:
-                col=0
-                row_sum=sum(abs_func_matrix[row])-abs_func_matrix[row][row]
-                while col<total_col:
-                        if row!=col:
-                                func_matrix[row][col]=func_matrix[row][col]/row_sum*abs_func_matrix[row][row]
-                        col+=1
-                row+=1
-        return func_matrix
+	abs_func_matrix=np.absolute(func_matrix)
+	total_col,total_row=func_matrix.shape()
+	row=0
+	while row<total_row:
+		col=0
+		row_sum=sum(abs_func_matrix[row])-abs_func_matrix[row][row]
+		while col<total_col:
+			if row!=col:
+				func_matrix[row][col]=func_matrix[row][col]/row_sum*abs_func_matrix[row][row]
+			col+=1
+		row+=1
+	return func_matrix
 
 def subn_similarity(cell_matrix,tissue_matrix):
-        t_trace=tissue_matrix.trace()[0]
-        c_trace=cell_matrix.trace()[0]
-        total_col,total_row=cell_matrix.shape()
-        row=0
-        while row<total_row:
-                cell_matrix[row][row]=cell_matrix[row][row]/c_trace*t_trace
-                row+=1
-        row=0
-        cell_matrix=normalize_matrix(cell_matrix)
-        tissue_matrix=normalize_matrix(tissue_matrix)
-        difference=0.0
-        while row<total_row:
-                col=0
-                while col<total_col:
-                        if col!=row:
-                                difference+=abs(cell_matrix[row][col]-tissue_matrix[row][col])
-                        col+=1
-                row+=1
-        return difference
+	t_trace=tissue_matrix.trace()[0]
+	c_trace=cell_matrix.trace()[0]
+	total_col,total_row=cell_matrix.shape()
+	row=0
+	while row<total_row:
+		cell_matrix[row][row]=cell_matrix[row][row]/c_trace*t_trace
+		row+=1
+	row=0
+	cell_matrix=normalize_matrix(cell_matrix)
+	tissue_matrix=normalize_matrix(tissue_matrix)
+	difference=0.0
+	while row<total_row:
+		col=0
+		while col<total_col:
+			if col!=row:
+				difference+=abs(cell_matrix[row][col]-tissue_matrix[row][col])
+			col+=1
+		row+=1
+	return difference
 
 
 if __name__ == '__main__':
 ######################################################
 #load tissue data
 ######################################################
+	tissue_graph=nx.Graph()
 	DSCN_load.load_omic_data(t_exp_file,t_exp_hash)
 	DSCN_load.load_omic_data(t_fc_file,t_fc_hash)
+	DSCN_load.load_common_gene_to_graph(ppi_file,tissue_graph)
+	load_cluster(cluster_file,cluster_list,tissue_graph)
+	DSCN_load.add_weight_to_sub_network(tissue_graph,cluster_list)
 ######################################################
 #load cellline data
 ######################################################
+	cell_graph=nx.Graph()
 	DSCN_load.load_omic_data(c_exp_file,c_exp_hash)
 	DSCN_load.load_omic_data(c_essen_file,c_essen_hash)
-#target_list=load_target(target_file)
 	DSCN_load.load_common_gene_to_graph(ppi_file,cell_graph)
+	#DSCN_load.add_weight_to_sub_network(cell_graph,cluster_list)
+	load_cluster(cluster_file,cluster_list,cell_graph)
 	DSCN_load.add_weight_to_sub_network(cell_graph,cluster_list)
 ############################################################
 #only calculate correlations occured within subnetwork
 ############################################################
+	for subn in cluster:
+		t_matrix=make_tc_matrix(tissue_graph.subgraph(subn),t_exp_hash,t_fc_hash)
+		c_matrix=make_tc_matrix(cell_graph.subgraph(subn),c_exp_hash,c_essen_hash)
+		nt_matrix=normalize_matrix(t_matrix)
+		nc_matrix=normalize_matrix(c_matrix)
+		print (subn_similarity(nc_matrix,nt_matrix))
